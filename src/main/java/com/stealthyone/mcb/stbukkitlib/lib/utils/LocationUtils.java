3a277b8e1376d6a2dcfdb557a38d5a66f690cc57
Stealth2800 <stealth2800@stealthyone.com>
2014-01-19 00:24:01 -0800

    Updated LocationUtils and PlayerUtils

--- src/main/java/com/stealthyone/mcb/stbukkitlib/lib/utils/LocationUtils.java
+++ src/main/java/com/stealthyone/mcb/stbukkitlib/lib/utils/LocationUtils.java
@@ -1,4 +1,37 @@
 package com.stealthyone.mcb.stbukkitlib.lib.utils;
 
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+
 public class LocationUtils {
+
+    public static String locationToString(Location location, boolean ignoreYawPitch) {
+        String returnString = location.getWorld().getName() + "," + location.getX() + "," + location.getY() + "," + location.getZ();
+        if (!ignoreYawPitch) {
+            returnString = returnString + "," + location.getYaw() + "," + location.getPitch();
+        }
+        return returnString;
+    }
+
+    public static Location stringToLocation(String rawLocation) {
+        String[] split = rawLocation.split(",");
+        if (split.length == 6) {
+            return new Location(
+                    Bukkit.getWorld(split[0]),
+                    Double.parseDouble(split[1]),
+                    Double.parseDouble(split[2]),
+                    Double.parseDouble(split[3]),
+                    Float.parseFloat(split[4]),
+                    Float.parseFloat(split[5])
+            );
+        } else {
+            return new Location(
+                    Bukkit.getWorld(split[0]),
+                    Double.parseDouble(split[1]),
+                    Double.parseDouble(split[2]),
+                    Double.parseDouble(split[3])
+            );
+        }
+    }
+
 }
--- src/main/java/com/stealthyone/mcb/stbukkitlib/lib/utils/PlayerUtils.java
+++ src/main/java/com/stealthyone/mcb/stbukkitlib/lib/utils/PlayerUtils.java
@@ -1,4 +1,20 @@
 package com.stealthyone.mcb.stbukkitlib.lib.utils;
 
+import com.stealthyone.mcb.stbukkitlib.api.Stbl;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+
+import java.util.UUID;
+
 public class PlayerUtils {
+
+    public static Player getPlayerByUuid(UUID uuid) {
+        return getPlayerByUuid(uuid.toString());
+    }
+
+    public static Player getPlayerByUuid(String uuid) {
+        String name = Stbl.getUuidManager().getName(uuid);
+        return name == null ? null : Bukkit.getPlayerExact(name);
+    }
+
 }
